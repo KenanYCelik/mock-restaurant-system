e8b961ba07e4d6d10c53ea22c6833c1cfd2bbc6b
KenanYCelik <ckenan1337@gmail.com>
2021-02-28 01:57:33 -0500

    Initial setup and basic classes made

---
+++ src/Food.java
@@ -0,0 +1,6 @@
+public abstract class Food {
+    private String Name;
+    private Money price;
+    private int calories;
+
+}
---
+++ src/Money.java
@@ -0,0 +1,34 @@
+public class Money {
+    private int dollars;
+    private int cents;
+
+    public Money(int dollars, int cents){
+        this.dollars = dollars;
+        this.cents = cents;
+    }
+
+    public Money add(Money money){
+        int totalCents = this.cents + money.cents;
+        int dollars = this.dollars + money.dollars + (totalCents/100);
+        return new Money(dollars, totalCents % 100);
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if(obj == this) return true;
+        if(!(obj instanceof Money)) return false;
+        final Money ob = (Money) obj;
+        return this.dollars == ob.dollars && this.cents == ob.cents;
+    }
+
+    @Override
+    public int hashCode() {
+        return (this.dollars * 100) + this.cents;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("$%d.%d", dollars, cents);
+    }
+
+}
